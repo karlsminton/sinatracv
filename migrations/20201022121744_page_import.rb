13f2aeb4bbed96e6be4d1e559592a884ed742a1e
karlsminton <ksm1nn0@gmail>
2022-07-11 17:31:12 +0100

    page migration working as intended

--- migrations/20201022121744_page_import.rb
+++ migrations/20201022121744_page_import.rb
@@ -5,10 +5,10 @@ class PageImport < ActiveRecord::Migration[7.0]
   def up
     path = Dir.glob('./conf/page_data.json').first
     file = File.read(path)
-    config = JSON.parse(file)
+    pages = JSON.parse(file)
 
-    config['data']['children'].each do |page|
-      Page.create(title: page.title, identifier: page.identifer, content: page.content)
+    pages['pages'].each do |page|
+      Page.create(title: page['title'], identifier: page['identifer'], content: page['content'])
     end
   end
 end
